aad474a10d1615e1998bfefcc6dfc4904cdc5bf9
Amanda_Katherine <walker.akw@gmail.com>
2020-08-24 16:14:35 -0400

    creates user and gem view files and creates GET '/signup' route in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-    
+    get '/signup' do
+        if logged_in? 
+            redirect to '/gems'
+        else 
+            erb :'users/signup'
+        end
+    end
+
+
+
+
 end
