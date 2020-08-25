6a77f9917b250cefb7482582d021b3fc69ae8403
Amanda_Katherine <walker.akw@gmail.com>
2020-08-25 00:00:29 -0400

    creates POST /gems request when a new gem is made in gem controller

--- app/controllers/gemstone_controller.rb
+++ app/controllers/gemstone_controller.rb
@@ -11,4 +11,26 @@ class GemstoneController < ApplicationController
             erb :'/gems/create_gem'
         end
     end
+
+    post '/gems' do
+        if logged_in? 
+            if params[:name] == "" || params[:description] == ""
+                #insert flash message about no blanks
+                redirect '/gems/new'
+            else
+                @gem = current_user.gems.build(name: params[:name], description: params[:description])
+                if @gem.save
+                    redirect "/gems/#{@gem.id}"
+                else
+                    redirect '/gems/new'
+                end
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    
+
+
 end
